74c2df07893696bafbfc6f37de0dd254e16d5a6c
turanct <spinnewebber_toon@hotmail.com>
2017-06-04 15:08:21 +0200

    :white_check_mark: Add missing test for WebHooksGeneric class
    forgot to commit...

---
+++ tests/WebHooksGenericTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Turanct\WebHooks;
+
+class WebHooksGenericTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException Turanct\WebHooks\WebHookWasNotSent
+     */
+    public function test_it_throws_when_connection_fails()
+    {
+        $client = $this->getMock('Turanct\\WebHooks\\HttpClient');
+        $client
+            ->method('execute')
+            ->will($this->throwException(new WebHookWasNotSent()));
+
+        $formatter = $this->getMock('Turanct\\WebHooks\\Formatter');
+        $formatter
+            ->method('format')
+            ->willReturn('foo');
+
+        $signer = $this->getMock('Turanct\\WebHooks\\Signer');
+        $signer
+            ->method('sign')
+            ->willReturn('bar');
+
+        $webhooks = new WebHooksGeneric($client, $formatter, $signer);
+        $webhooks->send(new WebHook('foo', 'bar', array('baz', 'qux')));
+    }
+
+    public function test_it_sends_an_http_request()
+    {
+        $expectedRequest = new HttpRequest(
+            'POST',
+            'http://foo.com/webhooks',
+            array(),
+            array(
+                'Content-Type' => 'application/json',
+                'X-Signature' => 'bar',
+            ),
+            'foo'
+        );
+
+        $client = $this->getMock('Turanct\\WebHooks\\HttpClient');
+        $client
+            ->expects($this->once())
+            ->method('execute')
+            ->with($this->equalTo($expectedRequest));
+
+        $formatter = $this->getMock('Turanct\\WebHooks\\Formatter');
+        $formatter
+            ->method('format')
+            ->willReturn('foo');
+        $formatter
+            ->method('getContentType')
+            ->willReturn('application/json');
+
+        $signer = $this->getMock('Turanct\\WebHooks\\Signer');
+        $signer
+            ->method('sign')
+            ->willReturn('bar');
+
+        $webhooks = new WebHooksGeneric($client, $formatter, $signer);
+        $webhooks->send(
+            new WebHook('http://foo.com/webhooks', 'bar', array('baz', 'qux'))
+        );
+    }
+}
