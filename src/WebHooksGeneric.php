9e5e9dfaf99a131347e5cf45d2e89db0175dbd49
turanct <spinnewebber_toon@hotmail.com>
2017-06-04 14:30:40 +0200

    :sparkles: Add Generic webhooks implementation for more configurability

---
+++ src/WebHooksGeneric.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Turanct\WebHooks;
+
+final class WebHooksGeneric implements WebHooks
+{
+    private $client;
+    private $formatter;
+    private $signer;
+
+    public function __construct(
+        HttpClient $client,
+        Formatter $formatter,
+        Signer $signer
+    ) {
+        $this->client = $client;
+        $this->formatter = $formatter;
+        $this->signer = $signer;
+    }
+
+    public function send(WebHook $webhook)
+    {
+        try {
+            $body = $this->formatter->format($webhook->getData());
+            $signature = $this->signer->sign($body, $webhook->getVerification());
+
+            $request = new HttpRequest(
+                'POST',
+                $webhook->getUrl(),
+                array(),
+                array(
+                    'Content-Type' => $this->formatter->getContentType(),
+                    'X-Signature' => $signature,
+                ),
+                $body
+            );
+
+            $this->client->execute($request);
+        } catch (HttpClientException $e) {
+            throw new WebHookWasNotSent('Webhook was not sent: connection failed', 0, $e);
+        }
+    }
+}
