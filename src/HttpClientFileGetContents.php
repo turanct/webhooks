35558c27b982f1e3d462e23d9fbe95a2012c14ff
Seppe Snoeck <seppe.snoeck@student.odisee.be>
2018-02-20 14:46:34 +0100

    Client for file_get_content

---
+++ src/HttpClientFileGetContents.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Turanct\WebHooks;
+
+final class HttpClientFileGetContents implements HttpClient
+{
+    public function execute(HttpRequest $request)
+    {
+        $headers = '';
+        foreach ($request->getHeaders() as $key => $value) {
+            $headers .= "{$key}: {$value}\r\n";
+        }
+
+        var_dump($headers);
+
+        $opts = array(
+            'http' => array(
+                'method'  => $request->getMethod(),
+                'header' => $headers,
+                'content' => $request->getBody()
+            ),
+            "ssl"=>array(
+                "verify_peer"=>false,
+                "verify_peer_name"=>false,
+            ),
+        );
+
+        $context  = stream_context_create($opts);
+
+        $result = file_get_contents($request->getUrl(), false, $context);
+        if ($result === false) {
+            throw new HttpClientException('Connection failed');
+        }
+        return new HttpResponse($http_response_header[0], $result, $http_response_header ?: array());
+    }
+}
